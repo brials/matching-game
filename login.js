c085628d42290ebcc36c7270533f64586c29be58
jenn083 <nguyenj1203@gmail.com>
2016-12-19 14:17:52 -0800

    added event listeners

--- login.js
+++ login.js
@@ -1,1 +1,27 @@
 var objArray = []; //We will set local storage into this.
+var objArray2 = [];
+
+
+function checkUsername() {
+
+}
+var username = document.getElementById('singIn').addEventListener('click', checkUsername);
+
+
+
+
+function checkPassword() {
+
+}
+
+var password = document.getElementById('password').addEventListener('click', checkPassword);
+
+
+
+if(!localStorage.getItem('jennifer')){
+  localStorage.setItem('jennifer',JSON.stringify(objArray));
+} else {
+  var retrievedData = localStorage.getItem('jennifer');
+  var objArray2 = JSON.parse(retrievedData);
+  localStorage.setItem('jennifer',JSON.stringify(objArray2));
+}
