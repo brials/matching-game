ce243ff97d668aab3c9442ca87b0ac999cd13b22
jenn083 <nguyenj1203@gmail.com>
2016-12-20 09:50:02 -0800

    login.js

--- login.js
+++ login.js
@@ -16,25 +16,28 @@ function loginHandler(event) {
   //Object
   //element objArray
   event.preventDefault();
-  var foundUser;
+  var foundUser=false;
   var user = event.target.username.value;
   var password = event.target.password.value;
   console.log(user);
   console.log(password);
   for (var i = 0; i < objArray.length;i++) {
     if(objArray[i].name === user){
-      if(objArray[i].password === password){
+      if(objArray[i].password === password) {
         objArray[i].active === true;
         foundUser = true;
       }
     }
   }
-  if(!foundUser){
-    return alert('Please re-enter a valid username or password. If you are a new user, please sign up.');
-  }
-  localStorage.objArray = JSON.stringify(objArray);//update local storage
-  window.location.href='matching.html';
 }
+if(!foundUser){
+  // var user = event.target.username.value;
+  // //var password = event.target.password.value;
+  // new People(user, password);
+  return alert('Please re-enter a valid username or password. If you are a new user, please sign up.');
+}
+localStorage.objArray = JSON.stringify(objArray);//update local storage
+window.location.href='matching.html';
 
 
 
@@ -47,8 +50,11 @@ function signUpHandler(event) {
   var user = event.target.username.value;
   var password = event.target.password.value;
   new People(user, password);
-
+  objArray[objArray.length - 1].active = true;
+  localStorage.objArray = JSON.stringify(objArray);//update local storage
+  window.location.href='matching.html';
 }
+
 
 document.getElementById('signUpSubmit').addEventListener('click', signUpHandler);
 
