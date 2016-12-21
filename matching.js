5c6f76ef32b7dcd53343cba12af8a7392caab7b3
brials <brials008@hotmail.com>
2016-12-21 00:53:14 -0800

    testing ai functionality not currently working

--- matching.js
+++ matching.js
@@ -4,12 +4,15 @@ var cardArray = []; // This will be the array of cards.
 var objArray = []; //This will be all of the users that we will pull from local storage.
 var tablePlace = document.getElementById('table');
 var scorePlace = document.getElementById('scores');
+var aiButtonPlace = document.getElementById('againstComp')
 var cardDown = 'images/Face_Down.png';
 var clickCount = 1;
 var userRows = 4;
 var startTime = new Date;
 var click1 = '';
 var click2 = '';
+var turn = 'user';
+var aiCardMatch = '';
 
 // function to render to page
 function rend(el, content, place, id, img, newClass){
@@ -41,12 +44,14 @@ function makeTable(rows){
 var names = ['Alki_Beach','Bruce_Brandon','Chess_Player','Chief_Seattle','Elephant_Carwash','Elliot_Bay_bookstore','Fremont_Troll','Great_Wheel','Hiram_Chittenden_Locks','International_District','Ivar_Seafood','Jazz_Alley','Microsoft','Mount_Rainer_National_Park','Museum_of_Flight','Original_Starbucks','Pacific_Science_Center','Pike_Place_Market','Pioneer_Square','Queen_Ann_Beerhall','Safeco_Field','Science_Center','Seahawks','Seattle_Aquarium','Seattle_Art_Museum','Sleepless_in_Seattle','Space_Needle','University_of_Washington','Washington_Park_Arboretum','Washington_State_Ferries','Waterfront','Woodland_Park_Zoo']
 
 //create object_ophelia
-function CardtoBeMatch(name) {
+function CardtoBeMatch(name, uniqueName) {
   this.name = name;
   this.imgLocation = 'images/'+name+'.png';
   this.faceUp = false;
   this.removed = false;
   this.location = '';
+  this.hasBeenSeen = false;
+  this.uniqueName = uniqueName;
   cardArray.push(this);
 }
 
@@ -54,14 +59,16 @@ function CardtoBeMatch(name) {
 function makeCards(rows){
   cardArray = [];
   for(var i = 0; i < rows * (rows / 2); i++) {
-    new CardtoBeMatch(names[i]);
-    new CardtoBeMatch(names[i]);
+    new CardtoBeMatch(names[i], names[i] + '1');
+    new CardtoBeMatch(names[i], names[i] + '2');
   }
 }
 
 // Resume button_ophelia
 document.getElementById('NewGame').addEventListener('click', function(){
   scorePlace.innerHTML = '';
+  tablePlace.addEventListener('click', tableHandler);
+  tablePlace.removeEventListener('click', aiTableHandler);
   userRows = parseInt(prompt('Enter your level: 2 or 4 or 6 or 8'));
   if(userRows != 2 && userRows != 4 && userRows != 6 && userRows != 8){
     return alert('Sorry, the answer must be either 2 or 4 or 6 or 8');
@@ -204,7 +211,138 @@ function tableHandler(event){
   }
 }
 
+//on ai button click
+function aiButtonHandler(){
+  scorePlace.innerHTML = '';
+  userRows = parseInt(prompt('Enter your level: 2 or 4 or 6 or 8'));
+  if(userRows != 2 && userRows != 4 && userRows != 6 && userRows != 8){
+    return alert('Sorry, the answer must be either 2 or 4 or 6 or 8');
+  }
+  tablePlace.removeEventListener('click',tableHandler);
+  tablePlace.addEventListener('click', aiTableHandler);
+  makeCards(userRows);
+  imageRandom(userRows);
+  renderImage();
+}
 
+//ai tablebuttonhandler.
+function aiTableHandler(event){
+  event.preventDefault();
+  if(turn === 'user'){
+    var eventId = event.target.id;
+    for(var c = 0; c < cardArray.length; c++){
+      if(eventId === cardArray[c].location){
+        var clickId = cardArray[c].name;
+      }
+    }
+    if(clickId === '' || clickId === 'table'){
+      return alert('Please click on an a remaining card.');
+    }
+    if(clickCount === 1){
+      for(var i = 0; i < cardArray.length; i++){
+        if(cardArray[i].location === eventId){
+          cardArray[i].faceUp = true;
+          cardArray[i].hasBeenSeen = true;
+          click1 = cardArray[i].name;
+          renderImage();
+        }
+      }
+    }
+    if(clickCount === 2){
+      for(var j = 0; j < cardArray.length; j++){
+        if(cardArray[j].location === eventId){
+          cardArray[j].faceUp = true;
+          cardArray[j].hasBeenSeen = true;
+          click2 = cardArray[j].name;
+          renderImage();
+        }
+      }
+      if(click1 === click2){
+        for(var b = 0; b < cardArray.length; b++){
+          if(cardArray[b].name === click1){
+            cardArray[b].removed = true;
+          }
+        }
+      }
+      for(var e = 0; e < cardArray.length; e++){
+        cardArray[e].faceUp = false;
+      }
+    }
+    setTimeout(function(){renderImage()}, 2000);
+    checkIfFinished();
+    if(clickCount === 1){
+      clickCount += 1;
+    } else {
+      clickCount = 1;
+    }
+  }
+  setTimeout(function(){compTurn()}, 5000);
+  setTimeout(function(){updateTurn()}, 5100);
+}
+
+//functions for ai handler only
+function isAMatch(element){
+  return element.uniqueName === aiCardMatch;
+}
+function remove(match1, match2){
+  cardArray[match1].removed = true;
+  cardArray[match2].removed = true;
+  setTimeout(function(){renderImage()}, 2000);
+}
+
+function compTurn(){
+  if(turn === 'comp'){
+    var foundMatch = false;
+    var match1 = 0;
+    var match2 = 0;
+    for(var o = 0; o < cardArray.length; o++){
+      var hold = cardArray[o].uniqueName;
+      if(hold.slice(-1) === 1){
+        hold = hold.slice(-1, 0) + '2';
+      } else {
+        hold = hold.slice(-1, 0) + '1';
+      }
+      aiCardMatch = hold;
+      if(cardArray.find(isAMatch)){
+        match1 = o;
+        foundMatch = true;
+        for(var u = 0; u < cardArray.length; u++){
+          if(cardArray[u].uniqueName === aiCardMatch){
+            match2 = u;
+          }
+        }
+      }
+    }
+    if(random(1) === 0 && foundMatch){
+      cardArray[match1].faceUp = true;
+      cardArray[match2].faceUp = true;
+      renderImage();
+      setTimeout(function(){remove(match1, match2)}, 2000)
+    } else {
+      match1 = random(cardArray.length);
+      match2 = random(cardArray.length);
+      while(match1 === match2){
+        match2 = random(cardArray.length);
+      }
+      cardArray[match1].faceUp = true;
+      cardArray[match2].faceUp = true;
+      renderImage();
+      cardArray[match1].faceUp = false;
+      cardArray[match2].faceUp = false;
+      setTimeout(function(){renderImage()}, 2000);
+    }
+  }
+}
+
+function updateTurn(){
+  if(clickCount === 2 && turn === 'user'){
+    clickCount = 1;
+    turn = 'comp';
+  } else {
+    turn = 'user';
+    clickCount += 1;
+  }
+}
 //function call
 
 // Part of localStorage
@@ -212,4 +350,5 @@ if(localStorage.objArray){
   objArray = JSON.parse(localStorage.objArray);
 }
 
-tablePlace.addEventListener('click', tableHandler);
+// tablePlace.addEventListener('click', tableHandler);
+aiButtonPlace.addEventListener('click', aiButtonHandler);
